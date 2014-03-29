be39d1b9ced309815705fb5dff2347b8405361e0
BrazosBrian <bdixon@anthc.org>
2014-03-28 16:11:20 -0800

    Add all files

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Best to break up application by features instead of what is below
+//
+// Declare app level module which depends on filters, and services
+var eventsApp = angular.module('eventsApp', []);
